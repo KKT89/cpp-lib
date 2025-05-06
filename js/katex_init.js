53757e8f83b009a66799077a08d96847bc9853c9
KKT89 <KKT89@users.noreply.github.com>
2025-05-06 07:00:27 +0000

    deploy: f4d4a8c48b6a9de031b2a68c5282a125080bd55d

---
+++ js/katex_init.js
@@ -0,0 +1,12 @@
+// ページ遷移のたびに実行
+document$.subscribe(({ body }) => {
+    renderMathInElement(body, {
+      delimiters: [
+        { left: "$$", right: "$$", display: true },
+        { left: "$",  right: "$",  display: false },
+        { left: "\\(", right: "\\)", display: false },
+        { left: "\\[", right: "\\]", display: true }
+      ]
+    });
+  });
+  
